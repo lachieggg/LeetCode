865ceaf2314a224300bc1faa4cab9d634860b7d7
Lachlan Grant <grant.lachlan.j@gmail.com>
2022-02-27 15:03:52 +1100

    Formatting x and y as binary integers

--- dynamicArray.py
+++ dynamicArray.py
@@ -26,24 +26,37 @@ QUERY_TYPE_INDEX = 0
 X_INDEX = 1
 Y_INDEX = 2
 
+
+def xor(x, y):
+    print('{0:08b}'.format(x))
+    print('{0:08b}'.format(y))
+
 def dynamicArray(n, queries):
+    # Construct the 2D array
     arr = []
     for index in range(n):
         arr.append([])
     
     lastAnswer = 0
     print(arr)
+    x = queries[X_INDEX]
+    y = queries[Y_INDEX]
 
+    xor(x, y)
     if(queries[QUERY_TYPE_INDEX] == FIRST_QUERY_INT):
         # Execute first query algorithm
-    elif(queryies[QUERY_TYPE_INDEX == SECOND_QUERY_INT):
+        pass
+    elif(queries[QUERY_TYPE_INDEX] == SECOND_QUERY_INT):
         # Execute second query algorithm
+        pass
     else:
         pass
     
 
-dynamicArray(10, None)
+dynamicArray(10, [1, 3, 5])
 exit()
+
+
 
 def main():
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
