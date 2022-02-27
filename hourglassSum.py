7799fce56e7436c2aaf2ed895fccdcc05b4aac64
Lachlan Grant <grant.lachlan.j@gmail.com>
2022-02-27 12:41:47 +1100

    Simplified code

--- hourglassSum.py
+++ hourglassSum.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 def getSubArray(arr, startX, startY):
     subArray = [[0,0,0],[0,0,0],[0,0,0]]
     for y in range(startY, startY + 3):
@@ -51,25 +49,37 @@ def hourglassSum(arr):
 
 
 
+matrixOne = [
+        [1,1,1,0,0,0], 
+        [0,1,0,0,0,0], 
+        [1,1,1,0,0,0],
+        [0,0,2,4,4,0], 
+        [0,0,0,2,0,0], 
+        [0,0,1,2,4,0]
+]
 
-arr = [[1,1,1,0,0,0], [0,1,0,0,0,0], [1,1,1,0,0,0],
-       [0,0,2,4,4,0], [0,0,0,2,0,0], [0,0,1,2,4,0]]
-
-
-arr =   [[-1, -1,  0, -9, -2, -2],
+matrixTwo = [
+        [-1, -1,  0, -9, -2, -2],
         [-2, -1, -6, -8, -2, -5],
         [-1, -1, -1, -2, -3, -4],
         [-1, -9, -2, -4, -4, -5],
         [-7, -3, -3, -2, -9, -9],
-        [-1, -3, -1, -2, -4, -5]]
-
-arr =  [[-1, 1, -1, 0, 0, 0],
-        [0, -1,  0, 0, 0, 0],
-        [-1, -1, -1, 0, 0, 0],
-        [0, -9, 2, -4, -4, 0],
-        [-7, 0, 0, -2, 0, 0],
-        [0, 0, -1, -2, -4, 0]]
+        [-1, -3, -1, -2, -4, -5]
+]
 
 
-maxHourGlassSum = hourglassSum(arr)
-print(maxHourGlassSum)
+matrixThree =  [
+        [-1,  1, -1,  0,  0,  0],
+        [ 0, -1,  0,  0,  0,  0],
+        [-1, -1, -1,  0,  0,  0],
+        [ 0, -9,  2, -4, -4,  0],
+        [-7,  0,  0, -2,  0,  0],
+        [ 0,  0, -1, -2, -4,  0]
+]
+
+
+inputArraySet = [matrixOne, matrixTwo, matrixThree]
+
+for arr in inputArraySet:
+    maxHourGlassSum = hourglassSum(arr)
+    print(maxHourGlassSum)
