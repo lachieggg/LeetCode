442226113a7793838a37c798f33d6d2146ee4485
Lachlan Grant <grant.lachlan.j@gmail.com>
2022-03-01 21:06:29 +1100

    Added plan for magic square

---
+++ magicSquare/magicSquare.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Plan:
+#
+# Take the input square
+# Compute all the current totals of each
+# row/column/diagonal
+#
+# Sort the totals by number of occurrences
+#
+# Then manipulate the sqauare
+# into fitting each of the magic numbers,
+# beginning with the magic number that appears
+# most frequently in the rows/columns/diagonals
+# of S
+#
+#
+
+
