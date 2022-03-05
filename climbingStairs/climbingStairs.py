1f2c86b2f22bfd969d5cc55eb53a6515cd9d6162
Lachlan Grant <grant.lachlan.j@gmail.com>
2022-03-05 18:56:52 +1100

    Update

---
+++ climbingStairs/climbingStairs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+class Solution(object):
+	def __init__(self):
+		self.results = {}
+
+	def initialize(self, n):
+		for x in range(1,n-1):
+			print(x)
+			self.results[x] = self.climbStairs(x)
+			print(self.results)
+
+	def climbStairs(self, n):
+		"""
+		:type n: int
+		:rtype: int
+		"""
+		if(self.results == {}):
+			self.initialize(n)
+
+		if(n == 1):
+			return 1
+		if(n == 2):
+			return 2
+
+		if(n in self.results.keys()):
+			return self.results.get(n)
+
+		return self.climbStairs(n-1) + self.climbStairs(n-2)
+
+
+s = Solution()
+s.initialize(45)
+print(s.climbStairs(45))
